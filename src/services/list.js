e1155dffb463c551079431ed9bfa5ea83011ea30
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-02-24 00:16:08 +0100

    add post request to save data to the server

--- src/services/list.js
+++ src/services/list.js
@@ -2,3 +2,14 @@ export async function getList() {
   const data = await fetch("http://localhost:3333/list");
   return await data.json();
 }
+
+export async function setItem (item) {
+  const data = await fetch("http://localhost:3333/list", {
+    method: 'POST',
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    body: JSON.stringify({ item })
+  });
+  return await data.json();
+}

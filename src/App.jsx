98c452ed097a57ee869f7545f40bb7e39ee4f1e9
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-02-23 23:08:23 +0100

    setup grocery list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { useState } from 'react'
-nan
+import { useState, useEffect } from 'react'
+import "./App.css";
+import { getList } from "./services/list"
 
 function App() {
+  const [list, setList] = useState([]);
   
-  return (<></>)
+  useEffect(() => {
+    let mounted = true;
+    getList()
+      .then(items => {
+        mounted ? setList(items) : ''
+      })
+    return () => mounted = false;
+  },[])
+
+
+  return (
+    <>
+      <div className="wrapper">
+        <h1>My Grocery List</h1>
+        <ul>
+          {list.map(item => <li key={item.item}>{item.item}</li>)}
+        </ul>
+      </div>
+    </>
+  )
 }
 
 export default App

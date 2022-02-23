f5e6eb75ccc889c747374e233b4a8d4e832a4dc0
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-02-24 00:16:22 +0100

    add persistent variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,62 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import "./App.css";
-import { getList } from "./services/list"
+import { getList, setItem } from "./services/list"
 
 function App() {
   const [list, setList] = useState([]);
+  const [itemInput, setItemInput] = useState('')
+  const [alert, setAlert] = useState(false)
+  let mounted = useRef(true);
   
   useEffect(() => {
-    let mounted = true;
+    if (list.length && !alert) {
+      return;
+    }
     getList()
       .then(items => {
-        mounted ? setList(items) : ''
+        if (mounted.current) {
+          setList(items)
+        }
       })
-    return () => mounted = false;
-  },[])
+    return () => mounted.current = false;
+  },[alert, list])
 
+  useEffect(() => {
+    if (alert) {
+      setTimeout(() => {
+        setAlert(false)
+      },1000)
+    }
+  },[alert])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setItem(itemInput)
+      .then(() => {
+        if (mounted.current) {
+          setItemInput('')
+          setAlert(true)
+        }
+      })
+  }
 
   return (
     <>
       <div className="wrapper">
         <h1>My Grocery List</h1>
         <ul>
-          {list.map(item => <li key={item.item}>{item.item}</li>)}
+          {list.map(item => <li key={item.item + "a" + item.id}>{item.item}</li>)}
         </ul>
+
+
+        {alert && <h2>Submit Successful</h2>}
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="text">
+            <p>New Item</p>
+            <input type="text" id="text" onChange={event => setItemInput(event.target.value)} value={itemInput} />
+          </label>
+          <button type="submit"> Submit </button>
+        </form>
       </div>
     </>
   )
